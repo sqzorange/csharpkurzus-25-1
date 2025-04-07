57b50f54ecbfc97349be374146621ff8e0fa8cb2
sqzorange <fintus32@gmail.com>
2025-04-07 13:24:08 +0200

    Questions rekord létrehozása

--- QO1APY/Program.cs
+++ QO1APY/Program.cs
@@ -1,2 +1,8 @@
 ﻿
 Console.WriteLine("Hello, World!");
+
+Question question = new Question("Kérdés", new List<string> { "a", "b", "c" }, 1, "easy");
+Console.WriteLine(question.text);
+Console.WriteLine(question.listAnswers);
+Console.WriteLine(question.correctIndex);
+Console.WriteLine(question.difficulty);
